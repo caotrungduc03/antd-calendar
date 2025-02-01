6a050a9ef3ba32f2c4bd79224c7f68d4bbf11056
caotrungduc <trungdc2003@gmail.com>
2025-02-01 23:08:38 +0700

    feat: add tailwind css

--- src/components/AntdCalendar.tsx
+++ src/components/AntdCalendar.tsx
@@ -1,5 +1,11 @@
+import "./AntdCalendar.css";
+
 const AntdCalendar = () => {
-  return <div>Antd Calendar</div>;
+  return (
+    <div className="antd-calendar">
+      <div className="tw-text-xl tw-font-bold tw-text-red-900">Antd Calendar</div>
+    </div>
+  );
 };
 
 export default AntdCalendar;
